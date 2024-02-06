7f4c8f51779e18770df2a87d2e35ddade86d7e1a
maychen <429807825@qq.com>
2024-02-06 20:12:05 +0800

    he

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-print("大鱼")
-print("化身孤岛的鲸")
+print("周深-大鱼")
+print("周深-化身孤岛的鲸")
 flag=True
 if flag:
 	print("留在深圳过年")
 else:
 	print("去云南过年")
-=======
-print("周深-大鱼")
-print("周深-化身孤岛的鲸")
->>>>>>> a80b93e (fix bug 101)
+
